df6951540c0f7e10325e1ca1a7c897addd4d682b
magnybor <magnybor@gmail.com>
2017-10-18 12:55:15 +0200

    creation du serveur WebService

---
+++ src/main/java/com/cinevial/CinevialApplication.java
@@ -0,0 +1,12 @@
+package com.cinevial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinevialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinevialApplication.class, args);
+	}
+}
