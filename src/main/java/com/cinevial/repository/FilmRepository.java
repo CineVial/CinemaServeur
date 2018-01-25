60e05e58a126cfecc3026b7040582da271574fcc
Robin Greyl <robingreyl@gmail.com>
2018-01-25 19:12:59 +0100

    merge

--- src/main/java/com/cinevial/repository/FilmRepository.java
+++ src/main/java/com/cinevial/repository/FilmRepository.java
@@ -3,5 +3,9 @@ package com.cinevial.repository;
 import com.cinevial.entity.FilmEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FilmRepository extends JpaRepository<FilmEntity, Integer> {
+
+    List<FilmEntity> findAllByCodeCat(Integer codeCat);
 }
