370e5e27e946bc5981b2592fbead055359fbe3fd
magnybor <magnybor@gmail.com>
2018-01-25 11:38:32 +0100

    fix categorie id

--- src/main/java/com/cinevial/repository/CategorieRepository.java
+++ src/main/java/com/cinevial/repository/CategorieRepository.java
@@ -3,7 +3,7 @@ package com.cinevial.repository;
 import com.cinevial.entity.CategorieEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategorieRepository extends JpaRepository<CategorieEntity, Integer> {
+public interface CategorieRepository extends JpaRepository<CategorieEntity, String> {
 
     void deleteAllByCodeCat(String codeCat);
 }
