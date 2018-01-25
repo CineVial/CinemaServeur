73279d1adf638ccd8dd0aead2c20f09440406c02
magnybor <magnybor@gmail.com>
2018-01-25 10:27:06 +0100

    replaced Gson with Jackson

--- src/main/java/com/cinevial/controller/MainController.java
+++ src/main/java/com/cinevial/controller/MainController.java
@@ -2,7 +2,6 @@ package com.cinevial.controller;
 
 import com.cinevial.entity.*;
 import com.cinevial.repository.*;
-import com.google.gson.Gson;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.io.File;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,14 +22,12 @@ public class MainController {
     private final PersonnageRepository personnageRepository;
     private final RealisateurRepository realisateurRepository;
 
-    private final Gson gson;
 
     public MainController(ActeurRepository acteurRepository,
                           CategorieRepository categorieRepository,
                           FilmRepository filmRepository,
                           PersonnageRepository personnageRepository,
                           RealisateurRepository realisateurRepository) {
-         this.gson = new Gson();
         this.acteurRepository = acteurRepository;
         this.categorieRepository = categorieRepository;
         this.filmRepository = filmRepository;
@@ -87,13 +83,13 @@ public class MainController {
     @RequestMapping("/categories")
     public ResponseEntity getCategories() {
         List<CategorieEntity> categories = new ArrayList<>(categorieRepository.findAll());
-        return ResponseEntity.ok(gson.toJson(categories));
+        return ResponseEntity.ok(categories);
     }
 
     @RequestMapping("/films")
     public ResponseEntity getFilms() {
         List<FilmEntity> films = new ArrayList<>(filmRepository.findAll());
-        return ResponseEntity.ok(gson.toJson(films));
+        return ResponseEntity.ok(films);
     }
 
 
@@ -101,13 +97,13 @@ public class MainController {
     @RequestMapping("/personnages")
     public ResponseEntity getPersonnages() {
         List<PersonnageEntity> personnages = new ArrayList<>(personnageRepository.findAll());
-        return ResponseEntity.ok(gson.toJson(personnages));
+        return ResponseEntity.ok(personnages);
     }
 
     @RequestMapping("/realisateurs")
     public ResponseEntity getRealisateurs() {
         List<RealisateurEntity> realisateurs = new ArrayList<>(realisateurRepository.findAll());
-        return ResponseEntity.ok(gson.toJson(realisateurs));
+        return ResponseEntity.ok(realisateurs);
     }
 
     // GETTERS for one
@@ -115,19 +111,19 @@ public class MainController {
     @RequestMapping("/acteur")
     public ResponseEntity getActeurById(@RequestParam("a_id") Integer a_id) {
         ActeurEntity acteur = acteurRepository.findOne(a_id);
-        return ResponseEntity.ok(gson.toJson(acteur));
+        return ResponseEntity.ok(acteur);
     }
 
     @RequestMapping("/categorie")
     public ResponseEntity getCategorieById(@RequestParam("c_id") Integer c_id) {
         CategorieEntity categorie = categorieRepository.findOne(c_id);
-        return ResponseEntity.ok(gson.toJson(categorie));
+        return ResponseEntity.ok(categorie);
     }
 
     @RequestMapping("/film")
     public ResponseEntity getFilmById(@RequestParam("f_id") Integer f_id) {
         FilmEntity film = filmRepository.findOne(f_id);
-        return ResponseEntity.ok(gson.toJson(film));
+        return ResponseEntity.ok(film);
     }
 
     @RequestMapping("/personnage")
@@ -146,13 +142,13 @@ public class MainController {
             return ResponseEntity.badRequest().build();
         }
 
-        return ResponseEntity.ok(gson.toJson(personnage));
+        return ResponseEntity.ok(personnage);
     }
     
     @RequestMapping("/realisateur")
     public ResponseEntity getRealisateurById(@RequestParam("r_id") Integer r_id) {
         RealisateurEntity realisateur = realisateurRepository.findOne(r_id);
-        return ResponseEntity.ok(gson.toJson(realisateur));
+        return ResponseEntity.ok(realisateur);
     }
 
     // SETTERS
@@ -160,31 +156,31 @@ public class MainController {
     @RequestMapping("/acteur/update")
     public ResponseEntity saveOrUpdateActeur(@RequestBody ActeurEntity acteurEntity) {
         ActeurEntity acteur = acteurRepository.saveAndFlush(acteurEntity);
-        return ResponseEntity.created(URI.create("/acteur/" + acteur.getNoAct())).body(gson.toJson(acteur));
+        return ResponseEntity.created(URI.create("/acteur/" + acteur.getNoAct())).body(acteur);
     }
 
     @RequestMapping("/categorie/update")
     public ResponseEntity saveOrUpdateCategorie(@RequestBody CategorieEntity categorieEntity) {
         categorieEntity = categorieRepository.saveAndFlush(categorieEntity);
-        return ResponseEntity.created(URI.create("/categorie/" + categorieEntity.getCodeCat())).body(gson.toJson(categorieEntity));
+        return ResponseEntity.created(URI.create("/categorie/" + categorieEntity.getCodeCat())).body(categorieEntity);
     }
     
     @RequestMapping("/film/update")
     public ResponseEntity saveOrUpdateFilm(@RequestBody FilmEntity filmEntity) {
         filmEntity = filmRepository.saveAndFlush(filmEntity);
-        return ResponseEntity.created(URI.create("/film/" + filmEntity.getNoFilm())).body(gson.toJson(filmEntity));
+        return ResponseEntity.created(URI.create("/film/" + filmEntity.getNoFilm())).body(filmEntity);
     }
 
     @RequestMapping("/personnage/update")
     public ResponseEntity saveOrUpdatePersonnage(@RequestBody PersonnageEntity personnageEntity) {
         personnageEntity = personnageRepository.saveAndFlush(personnageEntity);
-        return ResponseEntity.created(URI.create("/personnage/" + personnageEntity.getNoAct())).body(gson.toJson(personnageEntity));
+        return ResponseEntity.created(URI.create("/personnage/" + personnageEntity.getNoAct())).body(personnageEntity);
     }
 
     @RequestMapping("/realisateur/update")
     public ResponseEntity saveOrUpdateRealisateur(@RequestBody RealisateurEntity realisateurEntity) {
         realisateurEntity = realisateurRepository.saveAndFlush(realisateurEntity);
-        return ResponseEntity.created(URI.create("/realisateur/" + realisateurEntity.getNoRea())).body(gson.toJson(realisateurEntity));
+        return ResponseEntity.created(URI.create("/realisateur/" + realisateurEntity.getNoRea())).body(realisateurEntity);
     }
     
     // DELETE
